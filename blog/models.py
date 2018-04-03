81ad708a3c5facd2150a91627b133dde1c2bd2e5
alex <lxpollara@gmail.com>
2018-04-03 17:50:06 +0000

    mostly set format forblog post list page

--- blog/models.py
+++ blog/models.py
@@ -7,11 +7,11 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
+    image_src = models.CharField(max_length=200, null=True)
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
-
     def publish(self):
         self.published_date = timezone.now()
         self.save()
--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 
+
 # Create your views here.
 def post_list(request):
     posts = Post.objects.order_by('published_date')
-    print(posts)
     return render(request, 'postlist.html', {'posts': posts})
